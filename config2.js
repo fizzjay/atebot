92374a34d2b264edd8775fd8017ef800e5dfea64
fizzjay <jayyaj090@gmail.com>
2025-11-26 00:25:06 +0000

    Prepare local files

---
+++ config2.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // NOTE: To get a client secret/ID please email support@altavr.io. In that email include your in-game name and what you would like your bot to be called. = {
+      attConfig: {
+          clientId: 'client_95344acf-03de-4e44-ac07-0fcd920a782a',
+          clientSecret: 'zsyWXwTgigOS/NN99RFjtNom/Yn8Q8RybK7LDE5D05SACYWbbWhv3gL6ROGFFOh0e/gVT2lzDpmNp2vO7ij23Q==',
+          scope: [ 'ws.group', 'ws.group_members', 'ws.group_servers', 'ws.group_bans', 'group.info', 'group.join', 'group.leave', 'group.view', 'group.members', 'server.view', 'server.console'],
+          logVerbosity: 0,
+    },
+
+    userConfig: {
+      playerUsername: 'Wetdog.'
+    }
+  }
